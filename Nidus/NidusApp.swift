81db34ec6913121d31db2262e77286f1b084ef9b
SrAndrii <55akif5548@gmail.com>
2025-03-29 13:48:43 -0700

    Initial Commit

---
+++ Nidus/NidusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NidusApp.swift
+//  Nidus
+//
+//  Created by Andrii Liakhovych on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NidusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
